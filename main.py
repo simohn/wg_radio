d4d9f921345b0d0d32261f8a159e42178a3e91bc
Simohn <schauppinger@gmail.com>
2020-11-06 22:19:44 +0000

    Initial commit of Radio

---
+++ main.py
@@ -0,0 +1,27 @@
+from subprocess import Popen, PIPE
+from time import sleep
+
+class Radio:
+
+    def __init__(self):
+        self.playing = False
+
+    def play(self, url):
+        self.p = Popen(['mplayer', url], stdout=PIPE, stdin=PIPE, stderr=PIPE)
+        self.playing = True
+    
+    def stop(self):
+        if self.playing:
+            self.p.communicate(input="q".encode())
+            self.playing = False
+
+radio_stations_file = open("radio_stations.txt", "r")
+radio_stations = radio_stations_file.read().split("\n")
+
+radio = Radio()
+
+for url in radio_stations:
+    print("Start: " + url)
+    radio.play(url)
+    sleep(7)
+    radio.stop()
